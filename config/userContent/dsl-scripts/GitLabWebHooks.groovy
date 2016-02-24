ba53ac0015d8868549b0c24698278a815b1d001e
Adrián Lizano Navarro <adrian.lizano@ext.produban.com>
2016-02-24 10:49:19 +0100

    creation of proper web hooks

--- config/userContent/dsl-scripts/GitLabWebHooks.groovy
+++ config/userContent/dsl-scripts/GitLabWebHooks.groovy
@@ -1,12 +1,12 @@
-/*
-  GITLAB_PROJECT: GitLab project name, (like groupname/repositoryname)
+TLAB_PROJECT: GitLab project name, (like groupname/repositoryname)
   INITIAL_JOB_NAME: Jenkins project name
 */
 import jenkins.model.*
 
 // Input parameters
 def GITLAB_PROJECT = "${GITLAB_PROJECT}".trim()
-def INITIAL_JOB_NAME = "${INITIAL_JOB_NAME}".trim()
+def INITIAL_INTEGRATION_JOB_NAME = "${INITIAL_INTEGRATION_JOB_NAME}".trim()
+def INITIAL_RELEASE_JOB_NAME = "${INITIAL_RELEASE_JOB_NAME}".trim()
 
 def gitlab = Jenkins.getInstance().getDescriptor("com.dabsquared.gitlabjenkins.GitLabPushTrigger")
 def GITLAB_SERVER = gitlab.getGitlabHostUrl()
@@ -21,14 +21,25 @@ connection.connect()
 def text = connection.content.text
 assert connection.responseCode == 200
 
-webhook = Jenkins.getInstance().getRootUrl()+"project/"+INITIAL_JOB_NAME
-if (!text.contains("url\":\""+webhook+"\"")) {
-    url = new URL(GITLAB_SERVER+"/api/v3/projects/"+java.net.URLEncoder.encode(GITLAB_PROJECT)+"/hooks?private_token="+GITLAB_API_TOKEN+"&url="+webhook+
+webhook_integration = Jenkins.getInstance().getRootUrl()+"project/"+INITIAL_INTEGRATION_JOB_NAME
+webhook_release = Jenkins.getInstance().getRootUrl()+"project/"+INITIAL_RELEASE_JOB_NAME
+if (!text.contains("url\":\""+webhook_integration+"\"")) {
+    url = new URL(GITLAB_SERVER+"/api/v3/projects/"+java.net.URLEncoder.encode(GITLAB_PROJECT)+"/hooks?private_token="+GITLAB_API_TOKEN+"&url="+webhook_integration+
       "&merge_requests_events=true&push_events=true")
   connection = url.openConnection()
   connection.setRequestMethod("POST")
   connection.doOutput = true
   connection.connect()
   assert connection.responseCode == 201
-  println "New hook: "+webhook
+  println "New hook: "+webhook_integration
+}
+if (!text.contains("url\":\""+webhook_release+"\"")) {
+    url = new URL(GITLAB_SERVER+"/api/v3/projects/"+java.net.URLEncoder.encode(GITLAB_PROJECT)+"/hooks?private_token="+GITLAB_API_TOKEN+"&url="+webhook_release+
+      "&merge_requests_events=true&push_events=true")
+  connection = url.openConnection()
+  connection.setRequestMethod("POST")
+  connection.doOutput = true
+  connection.connect()
+  assert connection.responseCode == 201
+  println "New hook: "+webhook_release
 }
