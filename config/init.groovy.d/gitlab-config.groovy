8023708a50fd132ea19593da1fd8a9ae9ed62ccc
n90460 <fdelamor@isban.es>
2016-03-10 17:11:08 +0100

    Test connection and log results in gitlab and nexus config

--- config/init.groovy.d/gitlab-config.groovy
+++ config/init.groovy.d/gitlab-config.groovy
@@ -1,16 +1,31 @@
 import jenkins.model.*
+import java.util.logging.Logger
 
-println "-->Setting up git"
+def logger = Logger.getLogger("hudson.plugins.git.GitSCM")
 def inst = Jenkins.getInstance()
 def git = inst.getDescriptor("hudson.plugins.git.GitSCM")
 def email = (System.getenv("JENKINS_EMAIL") == null)? "jenkins@serenity.corp":System.getenv("JENKINS_EMAIL")
+logger.info("Git global config: {name=\"jenkins\", email=\"$email\"}")
 git.setGlobalConfigName("jenkins")
 git.setGlobalConfigEmail(email)
 git.save()
 
-println "-->Setting up GitLab"
-def gitlab = inst.getDescriptorByType(com.dabsquared.gitlabjenkins.GitLabPushTrigger.DescriptorImpl)
-gitlab.gitlabApiToken = System.getenv("GITLAB_API_TOKEN")
-gitlab.gitlabHostUrl = System.getenv("GITLAB_URL")
-gitlab.ignoreCertificateErrors = true
-gitlab.save()
+logger = Logger.getLogger("com.dabsquared.gitlabjenkins.GitLabWebHook")
+if (!(System.getenv("GITLAB_API_TOKEN")?.trim() && System.getenv("GITLAB_URL")?.trim())) {
+  logger.severe('GitLab environment variables not set. Gitlab access won\'t work')
+} else {
+  def gitlab = inst.getDescriptorByType(com.dabsquared.gitlabjenkins.GitLabPushTrigger.DescriptorImpl)
+  gitlab.gitlabApiToken = System.getenv("GITLAB_API_TOKEN")
+  gitlab.gitlabHostUrl = System.getenv("GITLAB_URL")
+  gitlab.ignoreCertificateErrors = true
+  logger.info("GitLab config: {url=\""+gitlab.getGitlabHostUrl()+"\", token=\""+gitlab.getGitlabApiToken().replaceAll('.', '*')+"\"}")
+  gitlab.save()
+
+  //testing connection
+  def url = new URL(gitlab.getGitlabHostUrl()+"/api/v3/projects?private_token="+gitlab.getGitlabApiToken())
+  def connection = url.openConnection()
+  connection.setRequestMethod("GET")
+  connection.connect()
+
+  (connection.responseCode == 200)? logger.info('Test GitLab API connection... Success'):logger.severe('Test GitLab API connection... ERROR '+connection.inputStream.withReader { Reader reader -> reader.text })
+}
--- config/init.groovy.d/maven-settings.groovy
+++ config/init.groovy.d/maven-settings.groovy
@@ -6,16 +6,15 @@ import com.cloudbees.plugins.credentials.Credentials;
 import com.cloudbees.plugins.credentials.CredentialsScope;
 import com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl;
 import com.cloudbees.plugins.credentials.SystemCredentialsProvider;
+import java.util.logging.Logger
 import jenkins.model.*;
-  
-println "-->Setting up maven"
 
+def logger = Logger.getLogger("org.apache.maven.artifact.deployer.DefaultArtifactDeployer")
 def mavenDeployerLogin = System.getenv("MAVEN_DEPLOYER_LOGIN")
 def mavenDeployerPasswd = System.getenv("MAVEN_DEPLOYER_PASSWD")
-
-if (mavenDeployerLogin == null && mavenDeployerPasswd == null){
-  println "WARNING: Not configuring Maven as 'mavenDeployerLogin' and 'mavenDeployerPasswd' environment variables are not set"
-}else{
+if (!(mavenDeployerLogin?.trim() && mavenDeployerPasswd?.trim())){
+  logger.severe("Maven deployer environment variables (MAVEN_DEPLOYER_LOGIN, MAVEN_DEPLOYER_PASSWD) are not set. Artifact deployment won't work")
+} else {
   //Create creds
   def mavenDeployerCredentialsId = "maven-deployer-credentials-id";
   def systemCreds = SystemCredentialsProvider.getInstance();
@@ -31,13 +30,14 @@ if (mavenDeployerLogin == null && mavenDeployerPasswd == null){
       mavenDeployerPasswd
       )
   )
-  
   systemCreds.save();
+  logger.info("Created system credential \"$mavenDeployerCredentialsId\": {username=\"$mavenDeployerLogin\",password=\""+mavenDeployerPasswd.replaceAll('.', '*')+"\"}")
   
   List<ServerCredentialMapping> serverCredentialMappings = new ArrayList<ServerCredentialMapping>();
   serverCredentialMappings.add(new ServerCredentialMapping("serenity", mavenDeployerCredentialsId));
  
-  InputStream is = new FileInputStream(Jenkins.instance.getRootDir().toString()+'/userContent/customConfigs/maven-settings.xml')
+  def settingsFile = Jenkins.instance.getRootDir().toString()+'/userContent/customConfigs/maven-settings.xml'
+  InputStream is = new FileInputStream(settingsFile)
     
   def mavenSettingsConfig = new MavenSettingsConfig(
     "org.jenkinsci.plugins.configfiles.maven.MavenSettingsConfig", 
@@ -46,6 +46,20 @@ if (mavenDeployerLogin == null && mavenDeployerPasswd == null){
   for (ConfigProvider provider : ConfigProvider.all()) {
       if (provider.isResponsibleFor(mavenSettingsConfig.id)) {
           provider.save(mavenSettingsConfig);
+          logger.info("Created maven settings file \"Serenity Maven Settings\" from \"$settingsFile\"")
       }
   }
+
+  // Test authentication
+  def nexusRepositoryUrl = System.getenv('NEXUS_BASE_URL')
+  if (nexusRepositoryUrl==null) {
+    nexusRepositoryUrl='http://islinnxp01.scisb.isban.corp:8081/nexus'
+  }
+  def url = new URL(nexusRepositoryUrl+"/service/local/authentication/login")
+  def connection = url.openConnection()
+  connection.setRequestMethod("GET")
+  connection.setRequestProperty("Authorization", "Basic "+(mavenDeployerLogin+':'+mavenDeployerPasswd).bytes.encodeBase64().toString());
+  connection.connect()
+
+  (connection.responseCode == 200)? logger.info('Test Nexus API connection... Success'):logger.severe('Test Nexus API connection... ERROR '+connection.inputStream.withReader { Reader reader -> reader.text })
 }
