695f2677408de4c9ba5e78e0f64c6eeb1a4ddcf7
xis16774 <odomingu@servexternos.isban.es>
2016-06-27 17:32:59 +0200

    Added OSE3_APP_NAME to PRE and PRO

--- config/userContent/dsl-scripts/nodejs_ABTesting.groovy
+++ config/userContent/dsl-scripts/nodejs_ABTesting.groovy
@@ -96,6 +96,7 @@ job (data[0]) {
           downstreamParameterized {
             trigger(deployPreJobName) {
               parameters {
+			    predefinedProp('OSE3_APP_NAME', data[3])
                 predefinedProp('OSE3_TEMPLATE_PARAMS',"${OSE3_TEMPLATE_PARAMS}")
                 predefinedProp('PIPELINE_VERSION','${FRONT_IMAGE_VERSION}')
               }
@@ -324,6 +325,7 @@ job (deployPreJobName) {
               parameters {
                 predefinedProp('OSE3_TEMPLATE_PARAMS','${OSE3_TEMPLATE_PARAMS}')
                 predefinedProp('PIPELINE_VERSION','${PIPELINE_VERSION}')
+				predefinedProp('OSE3_APP_NAME', '${OSE3_APP_NAME}')
               }
             }
           }
