2145e2876050d8d01ca50b202b2a83fd098f1b19
ALEGRE MARQUES JOSE ANTONIO <xIS16667@es.isban.com>
2016-08-05 13:01:21 +0200

    SALM-9: LDAP 'User search filter' not informed

--- config/init.groovy.d/ldap-config.groovy
+++ config/init.groovy.d/ldap-config.groovy
@@ -11,6 +11,7 @@ String groupSearchBase = System.getenv('LDAP_GROUP_BASE')
 String groupSearchFilter = System.getenv('LDAP_GROUP_FILTER')
 String managerDN = System.getenv('LDAP_BIND_DN')
 String managerPassword = System.getenv('LDAP_PASSWORD')
+String userSearchFilter = 'corpaliaslocal={0}'
 
 if (!(server?.trim() && userSearchBase?.trim() && groupSearchBase?.trim()
     && groupSearchFilter?.trim() && managerDN?.trim() && managerPassword?.trim())) {
@@ -23,7 +24,7 @@ boolean disableMailAddressResolver = false
 String displayNameAttributeName = 'cn'
 String mailAddressAttributeName = 'mail'
 
-SecurityRealm ldap_realm = new LDAPSecurityRealm(server, '', userSearchBase, '', groupSearchBase, groupSearchFilter, null, managerDN, Secret.fromString(managerPassword), inhibitInferRootDN, disableMailAddressResolver, null, null, displayNameAttributeName, mailAddressAttributeName)
+SecurityRealm ldap_realm = new LDAPSecurityRealm(server, '', userSearchBase, userSearchFilter, groupSearchBase, groupSearchFilter, null, managerDN, Secret.fromString(managerPassword), inhibitInferRootDN, disableMailAddressResolver, null, null, displayNameAttributeName, mailAddressAttributeName)
 Jenkins.instance.setSecurityRealm(ldap_realm)
 
 def list = System.getenv('LDAP_GROUPS')
