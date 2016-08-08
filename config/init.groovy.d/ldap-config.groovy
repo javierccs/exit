6f82fb2a00aaa784384e0e7850fd08548c7619bb
Jose A. Alegre <xIS16667@es.isban.com>
2016-08-08 10:49:39 +0200

    SALM-9: LDAP 'User search filter' and 'Display Name LDAP attribute'.

--- config/init.groovy.d/ldap-config.groovy
+++ config/init.groovy.d/ldap-config.groovy
@@ -11,7 +11,8 @@ String groupSearchBase = System.getenv('LDAP_GROUP_BASE')
 String groupSearchFilter = System.getenv('LDAP_GROUP_FILTER')
 String managerDN = System.getenv('LDAP_BIND_DN')
 String managerPassword = System.getenv('LDAP_PASSWORD')
-String userSearchFilter = 'corpaliaslocal={0}'
+String userSearchFilter = System.getenv('LDAP_USER_SEARCH')
+String displayNameAttributeName = System.getenv('LDAP_DISPLAY_NAME')
 
 if (!(server?.trim() && userSearchBase?.trim() && groupSearchBase?.trim()
     && groupSearchFilter?.trim() && managerDN?.trim() && managerPassword?.trim())) {
@@ -19,9 +20,17 @@ if (!(server?.trim() && userSearchBase?.trim() && groupSearchBase?.trim()
   return
 }
 
+if ( !(userSearchFilter?.trim() )) {
+  userSearchFilter = ''
+}
+
+if ( !(displayNameAttributeName?.trim() )) {
+  displayNameAttributeName = 'cn'
+}
+
 boolean inhibitInferRootDN = true
 boolean disableMailAddressResolver = false
-String displayNameAttributeName = 'cn'
+
 String mailAddressAttributeName = 'mail'
 
 SecurityRealm ldap_realm = new LDAPSecurityRealm(server, '', userSearchBase, userSearchFilter, groupSearchBase, groupSearchFilter, null, managerDN, Secret.fromString(managerPassword), inhibitInferRootDN, disableMailAddressResolver, null, null, displayNameAttributeName, mailAddressAttributeName)
