a7171e4ea635a3b2af9e83f0095c7a89a582942d
Iván Corrales <developer@wesovi.com>
2016-09-15 15:45:22 +0200

    feat: working on gitlab migration job

--- config/userContent/dsl-scripts/migrate_gitlab_from_v0_to_v1.groovy
+++ config/userContent/dsl-scripts/migrate_gitlab_from_v0_to_v1.groovy
@@ -6,8 +6,10 @@ def GITLAB_PROJECT_TARGET = "${GITLAB_PROJECT_TARGET}"
 def GITLAB_USER = "${GITLAB_USER}"
 def GITLAB_PASSWORD = "${GITLAB_PASSWORD}"
 
+out.println("Running groovy");
+
 job(buildJobName) {
-    label('')
+    //label('')
     logRotator(daysToKeep = 30, numToKeep = 10, artifactDaysToKeep = -1, artifactNumToKeep = -1)
     steps {
         shell("migrate_gitlab_from_v0_to_v1.sh ${GITLAB_PROJECT_SOURCE} ${GITLAB_PROJECT_TARGET} ${GITLAB_USER} ${GITLAB_PASSWORD}");
