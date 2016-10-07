d448eebbcf6570d46828952e49ef4c7570df8ecc
xis16774 <odomingu@servexternos.isban.es>
2016-10-07 13:55:17 +0200

    Added GitLab certificate to ca-certificates.crt

--- scripts/install-certificates.sh
+++ scripts/install-certificates.sh
@@ -8,3 +8,22 @@ do
    /usr/local/bin/install-cert.sh $url
 done
 update-ca-certificates
+CA_CERTS=/etc/ssl/certs/ca-certificates.crt
+if [[ $GITLAB_URL =~ (https?:\/\/)([^:^\/]*)(:[0-9]+)?(\/.*)? ]]; then
+    PROTOCOL=${BASH_REMATCH[1]}
+    SERVER=${BASH_REMATCH[2]}
+    PORT=${BASH_REMATCH[3]}
+    if [ -z "$PORT" ]; then
+      PORT="443"
+    else
+      PORT=${PORT:1}
+    fi
+    if [[ $PROTOCOL == "https://" ]]; then
+      echo "Adding GitLab certificate to $CA_CERTS"
+      echo $(echo -n | openssl s_client -showcerts -connect $SERVER:$PORT 2>/dev/null  | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p') >> $CA_CERTS
+      echo "GitLab certificate added to $CA_CERTS"
+    fi
+else
+  echo "[ERROR] unable to parse $1 URL"
+  return -1
+fi
