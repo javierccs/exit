c3d910b14b46815bb7e4880f64538c3d2ded09d1
XI318928 <XI318928@IS10460X318928P>
2016-12-14 14:02:16 +0100

    Create template liferay

---
+++ config/userContent/dsl-scripts/params_liferay_template_json_dev.groovy
@@ -0,0 +1,94 @@
+import net.sf.json.JSONObject;
+
+def jsonEditorOptions = JSONObject.fromObject(/{
+		disable_edit_json: true,
+        disable_properties: true,
+        no_additional_properties: true,
+        disable_array_add: true,
+        disable_array_delete: true,
+        disable_array_reorder: true,
+        theme: "bootstrap3",
+        iconlib:"foundation3",
+		schema: {
+	  "title": "Liferay OSE3 Template parameters - Development environment",
+	  "description" : "Customizable Liferay OSE3 template parameters (Do not use quotes). <br>The parameters in blank are leave with the PAAS's default values(consult PAAS page for more information)",
+	  "type": "object",
+		 "options": {
+			  "collapsed": true
+		   },	
+	  "properties": {
+ 		"CONFIGURATION_GIT_DEV": {
+			"type": "string",
+			"description": "Git repository for PHP and Apache configuration",
+			"default": "",
+                        "propertyOrder": 5
+		},
+		"CONFIGURATION_GIT_USER_DEV": {
+			"type": "string",
+			"description": "User of the git repository for PHP and Apache configuration",
+			"default": "",
+                        "propertyOrder": 5
+		},
+		"CONFIGURATION_GIT_PASSWORD_DEV": {
+			"type": "string",
+			"description": "Password of the git repository for PHP and Apache configuration",
+			"default": "",
+                        "propertyOrder": 5
+		},
+		"S3_BACKUP_HOST_DEV": {
+			"type": "string",
+			"description": "The S3 host fqdn where the backup is stored.",
+                        "propertyOrder": 6
+		}, 
+		"S3_BACKUP_BUCKET_DEV": {
+			"type": "string",
+			"description": "The S3 bucket where the backup is stored. (format: s3:\/\/bucket)",
+                        "propertyOrder": 7
+		},
+		"S3_BACKUP_ACCESS_KEY_DEV": {
+			"type": "string",
+			"description": "The S3 access key to download the backup.",
+                        "propertyOrder": 8
+		},
+		"S3_BACKUP_SECRET_KEY_DEV": {
+			"type": "string",
+			"description": "The S3 secret key to download the backup.",
+                        "propertyOrder": 9
+		}, 
+		"HTTP_PROXY_DEV": {
+			"type": "string",
+			"description": "Http Proxy environment variable.",
+			"propertyOrder": 10
+		}, 
+		"HTTPS_PROXY_DEV": {
+			"type": "string",
+			"description": "Https Proxy environment variable.",
+                        "propertyOrder": 11
+		}, 
+		"NO_PROXY_DEV": {
+			"type": "string",
+			"description": "No Proxy environment variable.",
+			"default": "s3.boae.paas.gsnetcloud.corp",
+                        "propertyOrder": 12
+		},
+		"TZ_DEV": {
+			"type": "string",
+			"description": "TimeZone for the running containers.",
+			"dafault": "Europe\/Madrid",
+                        "propertyOrder": 13
+		},
+		"IGNORELIST_DEV": {
+			"type": "string",
+			"description": "Bittorent Sync Ignore List. (Use comma separator)",
+                        "propertyOrder": 14
+		},
+		"CONTAINER_MEMORY_DEV": {
+			"type": "string",
+			"description": "Maximum memory for Btsync (value in Megabytes. You should not exceed your quota)", 
+			"default": "1024M",
+                        "propertyOrder": 16
+		}
+      }
+    }
+}
+/);
---
+++ config/userContent/dsl-scripts/params_liferay_template_json_pro.groovy
@@ -0,0 +1,95 @@
+import net.sf.json.JSONObject;
+
+def jsonEditorOptions = JSONObject.fromObject(/{
+		disable_edit_json: true,
+        disable_properties: true,
+        no_additional_properties: true,
+        disable_array_add: true,
+        disable_array_delete: true,
+        disable_array_reorder: true,
+        theme: "bootstrap3",
+        iconlib:"foundation3",
+		schema: {
+	  "title": "Liferay OSE3 Template parameters - Production environment",
+	  "description" : "Customizable Liferay OSE3 template parameters (Do not use quotes). <br>The parameters in blank are leave with the PAAS's default values(consult PAAS page for more information)",
+	  "type": "object",
+		 "options": {
+			  "collapsed": true
+		   },	
+	  "properties": {
+ 		"CONFIGURATION_GIT_PRO": {
+			"type": "string",
+			"description": "Git repository for PHP and Apache configuration",
+			"default": "",
+                        "propertyOrder": 1
+		},
+		"CONFIGURATION_GIT_USER_PRO": {
+			"type": "string",
+			"description": "User of the git repository for PHP and Apache configuration",
+			"default": "",
+                        "propertyOrder": 2
+		},
+		"CONFIGURATION_GIT_PASSWORD_PRO": {
+			"type": "string",
+			"description": "Password of the git repository for PHP and Apache configuration",
+			"default": "",
+                        "propertyOrder": 3
+		},
+		"S3_BACKUP_HOST_PRO": {
+			"type": "string",
+			"description": "The S3 host fqdn where the backup is stored.",
+                        "propertyOrder": 6
+		}, 
+		"S3_BACKUP_BUCKET_PRO": {
+			"type": "string",
+			"description": "The S3 bucket where the backup is stored. (format: s3:\/\/bucket)",
+                        "propertyOrder": 7
+		},
+		"S3_BACKUP_ACCESS_KEY_PRO": {
+			"type": "string",
+			"description": "The S3 access key to download the backup.",
+                        "propertyOrder": 8
+		},
+		"S3_BACKUP_SECRET_KEY_PRO": {
+			"type": "string",
+			"description": "The S3 secret key to download the backup.",
+                        "propertyOrder": 9
+		}, 
+		"HTTP_PROXY_PRO": {
+			"type": "string",
+			"description": "Http Proxy environment variable.",
+			"propertyOrder": 10
+		}, 
+		"HTTPS_PROXY_PRO": {
+			"type": "string",
+			"description": "Https Proxy environment variable.",
+                        "propertyOrder": 11
+		}, 
+		"NO_PROXY_PRO": {
+			"type": "string",
+			"description": "No Proxy environment variable.",
+			"default": "s3.boae.paas.gsnetcloud.corp",
+                        "propertyOrder": 12
+		},
+		"TZ_PRO": {
+			"type": "string",
+			"description": "TimeZone for the running containers.",
+			"dafault": "Europe\/Madrid",
+                        "propertyOrder": 13
+		},
+		"IGNORELIST_PRO": {
+			"type": "string",
+			"description": "Bittorent Sync Ignore List. (Use comma separator)",
+                        "propertyOrder": 14
+		},
+		"CONTAINER_MEMORY_PRO": {
+			"type": "string",
+			"description": "Maximum memory for Btsync (value in Megabytes. You should not exceed your quota)", 
+			"default": "2048",
+                        "propertyOrder": 16
+		}
+      }
+    
+    }
+}
+/);
